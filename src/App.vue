76c8a7c7c3cc2b37dd69cb8360329bd4c8cdb5aa
l2el3er <se56160134@gmail.com>
2023-05-31 21:51:28 +0700

    add refinitiv-ui

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ef-panel id="login-page" spacing>
+    <ef-loader v-if="loading"></ef-loader>
+    <template v-else>
+      <h1>{{ title }}</h1>
+      <ef-text-field placeholder="Username"></ef-text-field>
+      <ef-password-field placeholder="Password"></ef-password-field>
+      <div id="button-group">
+        <ef-button @click="login">Login</ef-button>
+        <ef-button>Cancel</ef-button>
+      </div>
+    </template>
+  </ef-panel>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
+  data() {
+    return {
+      title: 'Hello!',
+      loading: false,
+    };
+  },
+  methods: {
+    login() {
+      this.loading = true;
+
+      setTimeout(() => {
+        this.title = 'Done!';
+        this.loading = false;
+      }, 3000);
+    },
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#login-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 450px;
+  height: 200px;
+  margin: 40px auto;
+}
+
+#button-group {
+  margin: 10px 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import '@refinitiv-ui/elements/loader';
+import '@refinitiv-ui/elements/button';
+import '@refinitiv-ui/elements/panel';
+import '@refinitiv-ui/elements/text-field';
+import '@refinitiv-ui/elements/password-field';
+
+import '@refinitiv-ui/halo-theme/dark/imports/native-elements';
+import '@refinitiv-ui/elements/loader/themes/halo/dark';
+import '@refinitiv-ui/elements/button/themes/halo/dark';
+import '@refinitiv-ui/elements/panel/themes/halo/dark';
+import '@refinitiv-ui/elements/text-field/themes/halo/dark';
+import '@refinitiv-ui/elements/password-field/themes/halo/dark';
+
 createApp(App).mount('#app')
